3ff6621236c0ba56626333320adb2ac073f9d17a
myurik <msyf.business@gmail.com>
2026-02-17 00:16:48 -0300

    feat: Exercício c) do Cap2 concluído. Com isso, o Cap2 também foi concluído com sucesso.

--- cap2/exercícios/resp2_c/js/resp2_c.js
+++ cap2/exercícios/resp2_c/js/resp2_c.js
@@ -4,9 +4,13 @@ const outPromoTerceiroProduto = document.querySelector("#outPromoTerceiroProduto
 
 frm.addEventListener("submit", (e) => {
     const nomeProduto = frm.inProduto.value;
-    const preco = frm.inPreco.value;
+    const preco = Number(frm.inPreco.value);
     
+    const desconto = preco * 0.50;
+    const precoTotal = (preco * 3) - desconto;
     
-    
+    outTituloPromo.textContent = `${nomeProduto} - Promoção: Leve 3 por R$: ${precoTotal.toFixed(2)}`;
+    outPromoTerceiroProduto.textContent = `O 3° Produto custa apenas R$: ${desconto.toFixed(2)}`;
+
     e.preventDefault();
 })
